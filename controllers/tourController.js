491f333e30baea379bd0051341e4bb712a03b07e
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2025-11-27 01:49:00 +0200

    fixing best-tours adding startLocation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,7 +14,7 @@ const aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields =
-    'name,price,ratingsAverage,summary,difficulty,imageCover,startLocaion';
+    'name,price,ratingsAverage,summary,difficulty,imageCover,startLocation';
   next();
 };
 
