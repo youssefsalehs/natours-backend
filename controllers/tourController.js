662c6300571acf3da3927a2934a212afe4b225a7
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2025-12-11 20:04:28 +0200

    fix:update tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -83,7 +83,6 @@ const updateTour = catchAsync(async (req, res) => {
   if (req.body.startLocation)
     tourData.startLocation = JSON.parse(req.body.startLocation);
   if (req.body.locations) tourData.locations = JSON.parse(req.body.locations);
-  if (req.body.guides) tourData.guides = JSON.parse(req.body.guides);
 
   const uploadBuffer = (fileBuffer, folder) => {
     return new Promise((resolve, reject) => {
