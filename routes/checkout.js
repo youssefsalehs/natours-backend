960aaa671cbd7554b9fd33ec8f588f16271b6557
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2025-12-18 13:22:30 +0200

    checkout 2

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SK);
-// router.post('/create-checkout-session', async (req, res) => {
-//   const { tours } = req.body;
-//   const trips = tours.map((t) => ({
-//     priceData: {
-//       currency: 'USD',
-//       tourData: {
-//         name: t.name,
-//         imageCover: t.imageCover,
-//       },
-//       unit_amount: Math.round(t.price * 100),
-//     },
-//     quantity: t.persons,
-//   }));
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_type: ['card'],
-//     line_items: trips,
-//     mode: 'payment',
-//     success_url: 'https://localhost:5173/overview',
-//     cancel_url: 'https://localhost:5173/about',
-//   });
+router.post('/create-checkout-session', async (req, res) => {
+  const { tours } = req.body;
+  const trips = tours.map((t) => ({
+    priceData: {
+      currency: 'USD',
+      tourData: {
+        name: t.name,
+        imageCover: t.imageCover,
+      },
+      unit_amount: Math.round(t.price * 100),
+    },
+    quantity: t.persons,
+  }));
+  const session = await stripe.checkout.sessions.create({
+    payment_method_type: ['card'],
+    line_items: trips,
+    mode: 'payment',
+    success_url: 'https://localhost:5173/overview',
+    cancel_url: 'https://localhost:5173/about',
+  });
 
-//   res.status(200).json({
-//     id: session.id,
-//   });
-// });
+  res.status(200).json({
+    id: session.id,
+  });
+});
 module.exports = router;
