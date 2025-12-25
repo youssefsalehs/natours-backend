e4a6d20ddc636e9b9908e362c0ca901a46478c8a
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2025-12-25 14:02:25 +0200

    cors update

--- api/app.js
+++ api/app.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'https://natours-backend-six.vercel.app/',
+  'https://natours-app-zeta.vercel.app/',
 ];
 
 const cors = require('cors');
